62fea8f80833c94f9f63d9210507ccdcd9015ef8
abhisheksaran <abhishekbishnoi695@gmail.com>
2021-07-27 16:50:16 +0530

    requirements.txt-added

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +120,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, ‘static’)
-django_heroku.settings(locals())
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
