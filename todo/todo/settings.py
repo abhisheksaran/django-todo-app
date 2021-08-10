eb2b732edf5afefa71f93e2f6584c05fc0f2129a
abhisheksaran <abhishekbishnoi695@gmail.com>
2021-08-10 11:15:50 +0530

    Edited allowed hosts

--- todo/todo/settings.py
+++ todo/todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2yi7v!ymqjmhh)so9g58)c*d52eifu)v-k=8#j1ht3q)=avpmz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ.get('HOST_IP','127.0.0.1')]
 
 
 # Application definition
